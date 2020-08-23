7b352f6018449ed64022dc0fa3cdce4110d9fb1b
Victor Romero <romeroa.victor@yahoo.com>
2020-08-23 21:13:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,39 +1,10 @@
 def nyc_pigeon_organizer(data)
-  data = {
-    "Theo" => {
-      :color => ["purple", "grey"],
-      :gender => ["male"],
-      :lives => ["Subway"]
-    },
-    "Peter Jr." => {
-      :color => ["purple", "grey"],
-      :gender => ["male"],
-      :lives => ["Library"]
-    },
-    "Lucky" => {
-      :color => ["purple"],
-      :gender => ["male"],
-      :lives => ["Central Park"]
-    },
-    "Ms. K" => {
-      :color => ["grey", "white"],
-      :gender => ["female"],
-      :lives => ["Central Park"]
-    },
-    "Queenie" => {
-      :color => ["white", "brown"],
-      :gender => ["female"],
-      :lives => ["Subway"]
-    },
-    "Andrew" => {
-      :color => ["white"],
-      :gender => ["male"],
-      :lives => ["City Hall"]
-    },
-    "Alex" => {
-      :color => ["white", "brown"],
-      :gender => ["male"],
-      :lives => ["Central Park"]
-    }
-  }
-end
+final_hash = data.each_with_object({}) do |(key, value), final_array|
+  value.each do |innerkeys, names| 
+    names.each do |name|
+      final_array[name].uniq
+      final_array[name][key].uniq
+      final_array[name][key].push(innerkeys.to_s)
+    end 
+  end
+end 
