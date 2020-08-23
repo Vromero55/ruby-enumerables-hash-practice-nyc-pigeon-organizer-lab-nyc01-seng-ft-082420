c5d0db704f71bed93f4deaa813e53a66584c45c1
Victor Romero <romeroa.victor@yahoo.com>
2020-08-23 21:13:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,10 @@
 def nyc_pigeon_organizer(data)
 final_hash = data.each_with_object({}) do |(key, value), final_array|
-  value.each do |innerkeys, names| 
+  value.each do |innerkeys, names|
     names.each do |name|
       final_array[name].uniq
       final_array[name][key].uniq
       final_array[name][key].push(innerkeys.to_s)
-    end 
+    end
   end
 end 
